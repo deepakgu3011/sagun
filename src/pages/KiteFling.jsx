ccd2ee8d78faaab5e131d7572af15892128c00ef
Deepak Kumar <114341053+deepakgu3011@users.noreply.github.com>
2025-06-22 10:18:57 +0530

    Update KiteFling.jsx

--- src/pages/KiteFling.jsx
+++ src/pages/KiteFling.jsx
@@ -34,8 +34,11 @@ function KiteFlying() {
   //     </style>
   //   </div>
   // );
-    <iframe src="https://shopping-hubs.rf.gd/public" style="width:100%; height:100vh;"></iframe>
-
+ <iframe
+      src="https://shopping-hubs.rf.gd/public"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Shopping Hub"
+    />
 }
 
 export default KiteFlying;
